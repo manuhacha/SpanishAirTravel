4333d3d7849ad9b124d382f10783f4f109b736e7
Manuel Ibáñez Hacha <bymanuxxyt@hotmail.com>
2025-03-22 01:44:48 +0100

    Starting creating Leaflet Map

--- Front/src/app/Components/home/home.component.ts
+++ Front/src/app/Components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { OpenSkyService } from '../../Services/open-sky.service';
 import { HttpClient } from '@angular/common/http';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -10,26 +11,51 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent {
 
-  data:string = '';
+   //Declaramos nuestro mapa
+   private map!: L.Map;
+   //Declaramos latitud y longitud
+   private latitud:number = 0;
+   private longitud:number = 0;
 
   constructor(private service: OpenSkyService, private http: HttpClient) {  }
-
+  
+  //Actualizamos los datos y el mapa cada 10 segundos
   ngOnInit() {
-    this.getPlanes()
+    this.getPlanes();
+    console.log(this.latitud)
+    this.loadMap();
   }
+
 
   getPlanes() {
     this.service.getPlanes()
     .subscribe({
       next: (res) => {
-        console.log(res)
-        this.data = res.states
+        console.log(res.states[0])
+        this.latitud = res.states[0][6]
+        this.longitud = res.states[0][7]
       },
       error: (err) => {
         console.log(err)
       }
     })
   }
+
+  private loadMap() {
+
+      //Cargamos nuestro mapa con nuestra latitud y longitud
+    this.map = L.map('map').setView([this.latitud,this.longitud],13);
+
+    //Creamos capa de mosaicos para el mapa
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    //Ponemos marcador para mostrar más información
+    L.marker([this.latitud,this.longitud]).addTo(this.map)
+    .openPopup();
+
+    }
 
 }
 
---
+++ Front/src/app/Components/map/map.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import * as L from 'leaflet'
+
+@Component({
+  selector: 'app-map',
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent {
+
+ 
+
+  ngAfterViewInit() {
+  }
+
+  
+
+}
